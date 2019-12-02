75edd0bc2bc7bedd74c5d159ebe65dfb07f2758c
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-12-02 21:05:48 +0100

    Fix ODR violations

--- include/slowmath/detail/type_traits.hpp
+++ include/slowmath/detail/type_traits.hpp
@@ -7,6 +7,15 @@
 #include <cstdint>      // for [u]int(8|16|32|64)_t
 #include <type_traits>  // for integral_constant<>, make_unsigned<>, is_signed<>, is_integral<>, is_same<>, common_type<>
 
+#include <gsl/gsl-lite.hpp> // for gsl_CPP17_OR_GREATER
+
+
+#if gsl_CPP17_OR_GREATER
+# define SLOWMATH_DETAIL_CONSTEXPR_INLINE constexpr inline
+#else
+# define SLOWMATH_DETAIL_CONSTEXPR_INLINE constexpr static
+#endif // gsl_CPP17_OR_GREATER
+
 
 namespace slowmath
 {
@@ -28,12 +37,12 @@ template <bool V0, typename T0, typename... Ts> struct mconjunction_ { using typ
 template <typename T0, typename T1, typename... Ts> struct mconjunction_<true, T0, T1, Ts...> : mconjunction_<T1::value, T1, Ts...> { };
 template <typename T0, typename... Ts> struct mconjunction : mconjunction_<T0::value, T0, Ts...>::type { };
 
-template <typename... Vs> constexpr bool are_integral_arithmetic_types_v = mconjunction<is_nonbool_integral<Vs>...>::value;
+template <typename... Vs> SLOWMATH_DETAIL_CONSTEXPR_INLINE bool are_integral_arithmetic_types_v = mconjunction<is_nonbool_integral<Vs>...>::value;
 
-template <bool Signed, typename... Vs> constexpr bool have_same_signedness_0_v = mconjunction<std::integral_constant<bool, std::is_signed<integral_value_type<Vs>>::value == Signed>...>::value;
-template <typename... Vs> constexpr bool have_same_signedness_v = false;
-template <> constexpr bool have_same_signedness_v<> = true;
-template <typename V0, typename... Vs> constexpr bool have_same_signedness_v<V0, Vs...> = have_same_signedness_0_v<std::is_signed<integral_value_type<V0>>::value, Vs...>;
+template <bool Signed, typename... Vs> SLOWMATH_DETAIL_CONSTEXPR_INLINE bool have_same_signedness_0_v = mconjunction<std::integral_constant<bool, std::is_signed<integral_value_type<Vs>>::value == Signed>...>::value;
+template <typename... Vs> SLOWMATH_DETAIL_CONSTEXPR_INLINE bool have_same_signedness_v = false;
+template <> SLOWMATH_DETAIL_CONSTEXPR_INLINE bool have_same_signedness_v<> = true;
+template <typename V0, typename... Vs> SLOWMATH_DETAIL_CONSTEXPR_INLINE bool have_same_signedness_v<V0, Vs...> = have_same_signedness_0_v<std::is_signed<integral_value_type<V0>>::value, Vs...>;
 
 template <typename V> struct wider_type_;
 template <typename V> using wider_type = typename wider_type_<V>::type;
@@ -43,13 +52,13 @@ template <> struct wider_type_<std::int32_t> { using type = std::int64_t; };
 template <> struct wider_type_<std::uint8_t> { using type = std::uint32_t; };
 template <> struct wider_type_<std::uint16_t> { using type = std::uint32_t; };
 template <> struct wider_type_<std::uint32_t> { using type = std::uint64_t; };
-template <typename V> constexpr bool has_wider_type_v = sizeof(V) <= sizeof(std::uint32_t);
+template <typename V> SLOWMATH_DETAIL_CONSTEXPR_INLINE bool has_wider_type_v = sizeof(V) <= sizeof(std::uint32_t);
 template <typename V> struct has_wider_type : std::integral_constant<bool, has_wider_type_v<V>> { };
 
 template <typename EH, typename V> using result_t = typename EH::template result<V>;
 
-template <typename T> constexpr T min_v = std::numeric_limits<T>::min();
-template <typename T> constexpr T max_v = std::numeric_limits<T>::max();
+template <typename T> SLOWMATH_DETAIL_CONSTEXPR_INLINE T min_v = std::numeric_limits<T>::min();
+template <typename T> SLOWMATH_DETAIL_CONSTEXPR_INLINE T max_v = std::numeric_limits<T>::max();
 
 
 } // namespace detail
